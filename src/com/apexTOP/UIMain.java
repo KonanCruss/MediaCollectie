f2c04872cf52bd7d4ac6a32089ff89320fd8ca07
KonanCruss <konancruss@gmail.com>
2016-11-08 19:10:40 +0100

    Different Main added to show the UI

--- src/com/apexTOP/UIMain.java
+++ src/com/apexTOP/UIMain.java
@@ -1,5 +1,6 @@
 package com.apexTOP;
 
+import com.apexTOP.data.MediaObject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,8 +8,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class UIMain extends Application {
+    public static Stage UIStage;
+    public static ArrayList<MediaObject> mediaList;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,6 +20,9 @@ public class UIMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        UIStage = primaryStage;
+        mediaList = new ArrayList<>();
+
         Parent root = FXMLLoader.load(getClass().getResource("ui/main.fxml"));
         primaryStage.setScene(new Scene(root, 585, 395));
         primaryStage.setTitle("Datastructuren");
--- src/com/apexTOP/ui/ControllerMain.java
+++ src/com/apexTOP/ui/ControllerMain.java
@@ -1,21 +1,39 @@
 package com.apexTOP.ui;
 
+import com.apexTOP.UIMain;
+import com.apexTOP.data.MediaObject;
+import java.util.Date;
+
+import com.sun.javafx.collections.ObservableListWrapper;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class ControllerMain {
     public Button bSelectFolder;
     public ImageView imageView;
-    public TreeView treeView;
     public Button bSortImages;
     public TextField tbSearchImage;
     public Button bSearchImage;
+    public ListView<MediaObject> listView;
 
     public void bSelectFolderOnAction(ActionEvent actionEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Open folder with pictures");
 
+        File selectedFile = directoryChooser.showDialog(UIMain.UIStage);
+        if(selectedFile == null || selectedFile.listFiles() == null) return;
+
+        for(File f : selectedFile.listFiles())
+            if(f.isFile())
+                UIMain.mediaList.add(new MediaObject(f.getName(), (int)f.getTotalSpace(), new Date(f.lastModified()), f.getAbsoluteFile()));
+
+        listView.setItems(new ObservableListWrapper<>(UIMain.mediaList));
     }
 
     public void bSortImagesOnAction(ActionEvent actionEvent) {
@@ -23,7 +41,6 @@ public class ControllerMain {
     }
 
     public void bSearchImageOnAction(ActionEvent actionEvent) {
-
 
     }
 }
