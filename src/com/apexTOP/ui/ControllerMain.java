0b3565b95355f31286fe5434d5ab737d111a8ff5
KonanCruss <konancruss@gmail.com>
2016-11-08 14:26:02 +0100

    Base of the UI has been added.

---
+++ src/com/apexTOP/ui/ControllerMain.java
@@ -0,0 +1,29 @@
+package com.apexTOP.ui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
+
+public class ControllerMain {
+    public Button bSelectFolder;
+    public ImageView imageView;
+    public TreeView treeView;
+    public Button bSortImages;
+    public TextField tbSearchImage;
+    public Button bSearchImage;
+
+    public void bSelectFolderOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void bSortImagesOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void bSearchImageOnAction(ActionEvent actionEvent) {
+
+
+    }
+}
